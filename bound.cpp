fb61c249ba33629095e896b1cbe8a9da6e7a41db
Joyce <geojoyce934@gmail.com>
2016-12-26 22:45:14 +0530

    added the sort function and lower bound

--- bound.cpp
+++ bound.cpp
@@ -9,7 +9,10 @@ vector<int>::iterator it1,it2;
 v.push_back(1);
 v.push_back(2);
 v.push_back(4);
+sort(v.begin(),v.end());
 it1=upper_bound(v.begin(),v.end(),3);
-cout<<it1[0];
+it2=lower_bound(v.begin(),v.end(),2);
+cout<<"Lower bound of 2 is: "<<it2[0]<<endl;
+cout<<it1[0]<<endl;
 return 0;
 }
